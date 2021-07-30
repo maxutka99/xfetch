1c11feb6b694bbc1b21b0e01782b21c086996566
corruptmemry <88046785+corruptmemry@users.noreply.github.com>
2021-07-30 11:10:46 +0700

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,3 +1,11 @@
+const char* userText   = " USER";
+const char* osText     = "     OS";
+const char* kernelText = "KERNEL";
+const char* shellText  = "SHELL";
+const char* uptimeText = "KERNEL";
+const char* ramText    = "   RAM";
+
+
 const char *ascii[10] = { "     ___           ",
                          " ___/   \\___     ",
                          "/   '---'   \\     ",
